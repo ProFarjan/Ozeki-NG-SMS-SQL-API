84cba4e82a8aa7071864fe871580e5928ef76928
Farjan Hasan <farjanhasannias@gmail.com>
2021-05-08 12:01:43 +0600

    First commit

---
+++ public_html/Api.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/**
+ * API class provide all kinds of methods sending and receiving sms
+ *
+ * @package   api
+ * @copyright 2021 World University of Bangladesh (CIS)
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU GPL v3 or later
+ */
+
+class Api {
+
+    public function __construct()
+    {
+
+    }
+
+
+}
---
+++ public_html/DB.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * DB class provide database connection functionality
+ *
+ * @package   db
+ * @copyright 2021 World University of Bangladesh (CIS)
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU GPL v3 or later
+ */
+
+class DB
+{
+
+    protected static $DB;
+
+    public function __construct()
+    {
+
+    }
+
+
+
+
+}
